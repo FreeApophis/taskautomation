3145897a49882d836ac3fd832978b98c836c5b91
Thomas Bruderer <apophis@apophis.ch>
2013-07-10 00:01:38 +0200

    Initial commit
    * Version for Chili 2.8 and Redmine 1.1
    * Documentation
    * Example Rules

---
+++ Chili/lib/tasks/taskautomation.rb
@@ -0,0 +1,152 @@
+#*******************************************
+#
+# Flexible automatic transitions for chiliproject
+#
+# (C) Thomas Bruderer
+#
+# configuration is below
+#
+#*******************************************
+namespace :apophis do
+  class Rules
+    attr_accessor :name, :tracker, :status, :age, :valid
+    attr_accessor :conditions, :checker
+    attr_accessor :changes, :action
+    def initialize name, tracker, status, age_days
+      @name = name
+      @age = age_days
+
+      @tracker = Array.new
+      if tracker.kind_of?(Array)
+        tracker.each do | t |
+          tr = Tracker.find_by_name(t)
+          @tracker << tr unless tr == nil
+        end
+      else
+        if tracker == "*"
+          @tracker = Tracker.all
+        else
+          @tracker << Tracker.find_by_name(tracker)
+        end
+      end
+      @status = Array.new
+      if status.kind_of?(Array)
+        status.each do | s |
+          st = IssueStatus.find_by_name(s)
+          @status << st unless st == nil
+        end
+      else
+        if status == "*"
+          @status = IssueStatus.find(:all, :conditions => ["is_closed = false"])
+        else
+          @status << IssueStatus.find_by_name(status)
+        end
+      end
+      @valid = true
+      @conditions = Hash.new
+      @changes = Hash.new
+      @action = nil
+    end
+    def set_condition attribute, value
+      @conditions.store(attribute, value)
+    end
+    def set_action delegate
+      if delegate.kind_of?(Method)
+        @action = delegate
+      end
+    end
+    def set_checker delegate
+      if delegate.kind_of?(Method)
+        @checker = delegate
+      end
+    end
+    def invoke_action issue, rule
+      if @action != nil
+        @action.call(issue, rule)
+      end
+    end
+    def invoke_checker issue
+      if @checker != nil
+        return @checker.call(issue)
+      end
+      return true
+    end
+    def set_next_state status
+      @changes.store(:status_id, IssueStatus.find_by_name(status).id)
+    end
+    def change_attribute attribute, value
+      @changes.store(attribute, value)
+    end
+  end
+  def is_not_author issue
+    return issue.author_id != issue.assigned_to_id
+  end
+  def set_to_author issue, rule
+    rule.change_attribute(:assigned_to_id, issue.author_id)
+  end
+  def due_date_reached issue
+    if issue.due_date == nil
+      return false
+    end
+    return issue.due_date < Date.today
+  end
+  def debug_putser issue, rule
+#    puts issue.inspect
+    puts "Issue #{issue.id} has due date: #{issue.due_date}"
+  end
+
+#***** CONFIGURE HERE: Defining of all the rules which should be executed *****
+  def self.create_rules
+    result = Array.new
+
+#ADD OR REMOVE RULES HERE
+#***** Rule: Automatically closed after 30 days *****
+    rule = Rules.new("Automatically close resolved issues after 30 days", ["Task", " Bug", "Feature", "Support"], ["Resolved"], 30)
+     rule.set_next_state("Closed")
+    result << rule
+
+#***** Rule: Due day reached *****
+   rule = Rules.new("Due date reached, new due date set to 2 days in the future. Please handle this ticket immediatly", "*", ["New", "In Progress", "Feedback"], 0)
+      rule.set_checker(method(:due_date_reached))
+    rule.change_attribute(:due_date, (Date.today + 2).to_s)
+    result << rule
+
+#   Return all the rules
+    return result
+  end
+#***** END CONFIGURE *****
+  desc "Observes all tickets and executes actions based on rules"
+  task :taskobserve => :environment do
+    admin = User.find_by_id(1)
+    rules = create_rules
+    rules.each do | rule |
+      if (rule.valid)
+        Issue.find(:all, :conditions => ["status_id IN (?) and tracker_id IN (?) and updated_on < ?", rule.status, rule.tracker, (DateTime.now - rule.age)]).each do |issue|
+          fulfilled = true
+          rule.conditions.each { |attribute, value| fulfilled = false if (issue.send(attribute) != value) }
+          fulfilled = (fulfilled and rule.invoke_checker(issue))
+          #if all conditions are fulfilled
+          if fulfilled
+            puts "Handle Issue #{issue.id}"
+            # a rule can make something more complicated, for that we call the callback which is called.
+            rule.invoke_action issue, rule
+            # we now change all attributes according to the rule, this includes state changes
+            if rule.changes.count > 0
+              rule.changes.each do | attribute, value |
+                issue.write_attribute(attribute, value)
+              end
+
+              issue.journal_user = admin
+              issue.journal_notes = rule.name
+
+              issue.save
+            end
+          end
+        end
+        puts "Rule '#{rule.name}' executed"
+      else
+        puts "Rule '#{rule.name}' is invalid and has been skipped"
+      end
+    end
+    puts "Observe Tasks ended"
+  end end
--- README.md
+++ README.md
@@ -1,4 +1,89 @@
 taskautomation
 ==============
 
-taskautomation rake tasks for redmine and chili
+I think Redmine and Chili are missing an important feature. Automatic transitions.
+
+If certain criterias are met then an action should be done automatically.
+
+As an example, it often happens that after the Resolved state, the review is not done and the task is never closed until someone cleans it up. Often it would be enough to give them a usefull timeframe to make a comment, but close it automatically when this is passed.
+
+Here comes in Taskautomation.
+
+It is only intended for Admins and familiar with Ruby on Rails / rake or at least Ruby because the rules are written in the script itself.
+The source is the documentation, it is simple to understand and still powerfull.
+
+Installation:
+
+1. Copy taskautomation.rb into lib/tasks
+2. Write your rules
+3. Test with the rake command
+
+go to your Chili or Redmine directory
+rake RAILS_ENV=production apophis:taskobserve
+
+4. Setup a cron task
+
+This cron task checks every hour, this is even with active projects usually enough. You could of course only once per day.
+
+crontab -e
+
+Add:
+0 * * * * cd /var/www/<CHILI/REDMIN/PATH>/ && /usr/bin/rake RAILS_ENV=production apophis:taskobserve
+
+Writing rules:
+
+I wanted to make it as simple as possible for common cases, and still flexible enough:
+
+Simple example:
+
+This rule is probably the most used one:
+
+We create a new rules:
+1. argument: Name of the Rule, this text will be added as a comment to the tickets when the rule is executed on a ticket:
+2. argument: Lists which Workflows are selected (can be string if only 1 Worflow, or "*" if you want to match all non-closed states.)
+3. argument: Tasks in which state are selected (can be string if only 1 Worflow, or "*" if you want to match all non-closed states.)
+4. argument: wasnt touch for how many days?
+
+We set an action what should happen when this criteria is met: in this case the state should be changed to "Closed"
+
+#***** Rule: Automatically closed after 30 days *****
+    rule = Rules.new("Automatically close resolved issues after 30 days", ["Task", " Bug", "Feature", "Support"], ["Resolved"], 30)
+    rule.set_next_state("Closed")
+    result << rule
+	
+More complex example:
+
+For more complex examples you can write your own methods which you want to have executed.
+
+In this case we set a checker method which must return a boolean value.
+And we als set an action, instead of just set it to the next state.
+
+If a task is in state Feedback we assign it to the author because he should give feedback. In the states done and won't do its their responsibility to verify the result and close the ticket.
+
+#***** Rule: Enforce Author for State Needs Feedback, Done and Won't Do *****
+    rule =  Rules.new("Automaticaclly enforce assigned-to to the author of the ticket", "*", ["Done", "Won't Do", "Needs Feedback"], 0)
+    rule.set_checker(method(:is_not_author))
+    rule.set_action(method(:set_to_author))
+
+    result << rule
+	
+Here we see the very simple implementation. Which those two delegates you are able to execute arbitrary checks and transitions.
+
+The check is a very simple one-liner
+
+  def is_not_author issue
+    return issue.author_id != issue.assigned_to_id
+  end
+
+And here you see how can change arbitrary attributes.
+  
+  def set_to_author issue, rule
+    rule.change_attribute(:assigned_to_id, issue.author_id)
+  end
+  
+I know the usability is not very good, a webinterface would be nice. If you intend to create a better way I would be interested in feedback.
+
+Both scripts are or have been in active use, and did a good job.
+
+Please clone and fork.
+Apophis
